c63b144f09073f8b5584e2f3b6e568fd120a64f0
Hepik <hepiky@gmail.com>
2022-08-03 17:56:15 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,16 +3,10 @@ import random
 
 class Generator:
 
-    def __init__(self):
-        self.random_list_of_numbers = self.get_random_numbers()
-
     def get_random_numbers(self):
-        random_list_of_numbers = []
-        for i in range(0, 5):
-            n = random.randint(1, 100000)
-            random_list_of_numbers.append(n)
-        return random_list_of_numbers
+        while True:
+            number = random.randint(1, 100000)
+            yield number
 
 
 object1 = Generator()
-print(object1.random_list_of_numbers[1])
