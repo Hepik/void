388f4db44c6fea23e76dee68e8eb87aa377e1f74
Hepik <hepiky@gmail.com>
2022-08-04 13:19:20 +0300

    file manage
    made write csv format.

--- receiver.py
+++ receiver.py
@@ -1,3 +1,5 @@
+import csv
+
 import paho.mqtt.client as mqtt
 
 
@@ -6,15 +8,16 @@ class Receiver:
         super().__init__()
         self.client2 = mqtt.Client()
         self.client2.connect('localhost')
-        self.client2.subscribe('/numbers/#')
+        self.client2.subscribe('/number/#')
         self.client2.on_message = self.on_message
         self.client2.loop_forever()
 
     def on_message(self, client, userdata, msg):
         print(msg.topic + " " + str(msg.payload))
 
-        with open('Numbers', 'wb') as file:
-            file.write(msg.payload)
+        with open('numbers.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(msg.payload)
 
 
 receiver = Receiver()
