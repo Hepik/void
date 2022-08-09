902ba83f972408fb1fc07cdd5adb4bf54c8a0aec
Hepik <hepiky@gmail.com>
2022-08-09 10:17:31 +0300

    made class Generator abstract

--- generator.py
+++ generator.py
@@ -1,8 +1,9 @@
 import random
+from abc import ABC
 from time import sleep
 
 
-class Generator:
+class Generator(ABC):
     def __init__(self, sleep_time: int = 1, max_value: int = 1000000, min_value: int = 1):
         self.sleep_time = sleep_time
         self.max_value = max_value
